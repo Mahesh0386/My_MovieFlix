d01515cbc5fd45690865b5267314f7efbb22fdde
bhannur <mahesh.bhannur@gmail.com>
2018-04-23 16:26:50 -0400

    feat(module-api): add user and show(movie/series) entity

---
+++ server/src/main/java/io/bhannur/api/JPAConfig.java
@@ -0,0 +1,56 @@
+	package io.bhannur.api;
+
+    import org.eclipse.persistence.config.PersistenceUnitProperties;
+    import java.util.Properties;
+    import org.eclipse.persistence.logging.SessionLog;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.jdbc.datasource.DriverManagerDataSource;
+    import org.springframework.orm.jpa.JpaTransactionManager;
+    import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+    import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+    import org.springframework.transaction.PlatformTransactionManager;
+    import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+    import javax.persistence.EntityManagerFactory;
+    import javax.sql.DataSource;
+
+
+    @Configuration
+    @EnableTransactionManagement
+    public class JPAConfig {
+
+        @Bean
+        public LocalContainerEntityManagerFactoryBean createEmf() {
+            LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+            emf.setJpaVendorAdapter(new EclipseLinkJpaVendorAdapter());
+            emf.setPackagesToScan("io.bhannur.api.entity");
+            emf.setDataSource(dataSource());
+            emf.setJpaProperties(jpaProperties());
+            return emf;
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            DriverManagerDataSource ds = new DriverManagerDataSource();
+            ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            ds.setUrl("jdbc:mysql://localhost:3306/movieflix-db?useSSL=false");
+            ds.setUsername("root");
+            ds.setPassword("Denso@963");
+            return ds;
+        }
+
+        @Bean
+        public PlatformTransactionManager txnMgr(EntityManagerFactory emf) {
+            JpaTransactionManager txnMgr = new JpaTransactionManager(emf);
+            return txnMgr;
+        }
+
+        private Properties jpaProperties() {
+            Properties prop = new Properties();
+            prop.setProperty(PersistenceUnitProperties.WEAVING, "false");
+            prop.setProperty(PersistenceUnitProperties.DDL_GENERATION, PersistenceUnitProperties.CREATE_OR_EXTEND);
+            prop.setProperty(PersistenceUnitProperties.LOGGING_LEVEL, SessionLog.FINE_LABEL);
+            return prop;
+        }
+    }
---
+++ server/src/main/java/io/bhannur/api/entity/user.java
@@ -0,0 +1,59 @@
+package io.bhannur.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class user {
+
+    @Id
+    private String userID;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
